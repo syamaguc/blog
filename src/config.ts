37abc6a4cd7f87bdbd3e36a3064f71fce5222b01
syamaguc <programmer2844@gmail.com>
2025-11-21 13:57:23 +0900

    fix deploy error

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,9 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: false,
+    enabled: true,
+    text: "この記事をGitHubで編集",
+    url: "https://github.com/syamaguc/blog/edit/main/",
   },
   dynamicOgImage: false,
   dir: "ltr", // "rtl" | "auto"
